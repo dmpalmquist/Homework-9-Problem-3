934cc5cec5e16c166f5748a25f5f090810423f38
Dylan Palmquist <111770130+dmpalmquist@users.noreply.github.com>
2022-10-27 18:48:15 +0000

    prints the array

--- .ccls-cache/@home@runner@Homework-9-Problem-3/main.cpp
+++ .ccls-cache/@home@runner@Homework-9-Problem-3/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void printmatrix(char mymat[][5], int Num_rows, int Num_cols);
 
 int main() {
-  std::cout << "Hello World!\n";
+  char tandf[6][5] = {{'T', 'F', 'T', 'T', 'T'},
+                      {'T', 'T', 'T', 'T', 'T'},
+                      {'T', 'T', 'F', 'F', 'T'},
+                      {'F', 'T', 'F', 'F', 'F'},
+                      {'F', 'F', 'F', 'F', 'F'},
+                      {'T', 'T', 'F', 'T', 'F'}};
+
+  printmatrix(tandf, 6, 5);
+  
+}
+
+void printmatrix(char mymat[][5], int Num_rows, int Num_cols){
+  for (int r = 0; r < Num_rows; r++){
+    for (int c = 0; c < Num_cols; c++){
+      cout << setw(8) << mymat[r][c];
+    }
+  cout << endl;
+  }
 }
